d8c5055d381235be17f08bf84b16cf1662947bdf
ajturner <aturner@esri.com>
2014-08-14 23:43:56 -0400

    Working FeatureService and Preview - no extents

--- models/Elasticsearch.js
+++ models/Elasticsearch.js
@@ -5,7 +5,7 @@ exports.find = function( id, options, callback ){
   var type = 'Elasticsearch';
   var geojson = { type:'FeatureCollection', features:[] };
 
-  var url = 'http://localhost:9200/'+id+'/_search'; // <-- change this 
+  var url = 'http://localhost:9200/'+id+'/_search';
 
   request.get(url, function(e, res){
     // TODO: add error handling if invalid index - ajturner
@@ -13,9 +13,9 @@ exports.find = function( id, options, callback ){
         features = json.hits.hits;
     // do something; turn it into GeoJSON
     features.forEach(function(row,i){
-      feature = { type: 'Feature', properties: row['_source'].properties, geometry: row['_source'].geometry };
+      feature = { type: 'Feature', properties: JSON.parse(row['_source'].properties), geometry: row['_source'].geometry };
       geojson.features.push( feature );
     });
-        callback( null, geojson );
+    callback( null, [geojson] );
   });
 };
